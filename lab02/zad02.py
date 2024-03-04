5df8c9226a0ebf85da32d32029f1354789c6bf5c
maria_koren <maria_koren@yahoo.com>
2024-03-04 21:49:39 +0100

    final

--- lab02/zad02.py
+++ lab02/zad02.py
@@ -1,53 +1,35 @@
-import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.decomposition import PCA
+import pandas as pd
+import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-# Wczytanie danych IRIS
-iris = datasets.load_iris()
-X = iris.data
 
-# PCA z wszystkimi komponentami
-pca_all = PCA().fit(X)
+iris=datasets.load_iris()
+X=pd.DataFrame(iris.data, columns=iris.feature_names)
+y=pd.Series(iris.target, name='FlowerType')
 
-# Suma kumulatywna wariancji wyjaśnionej przez kolejne komponenty
-cumulative_variance_ratio = pca_all.explained_variance_ratio_.cumsum()
 
-# Znalezienie indeksu pierwszej składowej, która przekracza 95% wariancji
-# n_components = (cumulative_variance_ratio < 0.95).sum() + 1
-n_components = 3
-# PCA z odpowiednią liczbą komponentów
-pca = PCA(n_components=n_components)
-X_pca = pca.fit_transform(X)
+pca_iris3=PCA(n_components=3).fit(iris.data)
+print(pca_iris3)
+print(pca_iris3.explained_variance_ratio_) #[0.92461872 0.05306648 0.01710261]
 
-# Wyświetlenie wyniku
-print(f'Liczba komponentów do zachowania minimum 95% wariancji: {n_components}')
+pca_iris2=PCA(n_components=2).fit(iris.data)
+print(pca_iris2)
+print(pca_iris2.explained_variance_ratio_) #[0.92461872 0.05306648]
 
-# Przypisanie nazw gatunków do wartości 0, 1, 2
+#powyższe wyniki oznaczają że istotnych kolumn 2
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(iris.data)
+
 species_names = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
 custom_cmap = mcolors.ListedColormap(['red', 'green', 'blue'])
 
-# Wykres punktowy
-if n_components == 2:
-    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=iris.target, cmap=custom_cmap)
-    plt.xlabel('Principal Component 1')
-    plt.ylabel('Principal Component 2')
-    plt.title('PCA Plot (2 Components)')
-    # Dodanie legendy
-    legend_handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=species_names[i]) for i, color in enumerate(['red', 'green', 'blue'])]
-    plt.legend(handles=legend_handles)
-    plt.show()
-elif n_components == 3:
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=iris.target, cmap=custom_cmap)
-    ax.set_xlabel('Principal Component 1')
-    ax.set_ylabel('Principal Component 2')
-    ax.set_zlabel('Principal Component 3')
-    ax.set_title('PCA Plot (3 Components)')
-    # Dodanie legendy
-    legend_handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=species_names[i]) for i, color in enumerate(['red', 'green', 'blue'])]
-    ax.legend(handles=legend_handles)
-    plt.show()
-else:
-    print("Wykres nie jest obsługiwany dla liczby komponentów większej niż 3.")
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=iris.target, cmap=custom_cmap)
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.title('PCA Plot (2 Components)')
+# Dodanie legendy
+legend_handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=species_names[i]) for i, color in enumerate(['red', 'green', 'blue'])]
+plt.legend(handles=legend_handles)
+plt.show()
