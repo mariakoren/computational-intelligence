09e50cccb3484efc77014f1bc839750a408490ce
maria_koren <maria_koren@yahoo.com>
2024-03-11 18:23:24 +0100

    zad01

--- lab03/zad01.py
+++ lab03/zad01.py
@@ -37,19 +37,23 @@ print(good_predictions/len*100, "%")
 #wyszło 7 z 45 => 15.5%
 
 # print(train_set)
-# print(train_set[:, 4 ])
-setosa = []
-versicolor = []
-virginica = []
+# sorted_train_set = train_set[train_set[:, -1].argsort()]
+# print(sorted_train_set)
 
-for element in train_set:
-    if element[4] == "Setosa":
-        setosa.append(element)
-    elif element[4] == "Versicolor":
-        versicolor.append(element)
-    elif element[4] == "Virginica":
-        virginica.append(element)
 
-sorted_array = setosa + versicolor + virginica
+def classify_iris_2(sl, sw, pl, pw):
+    if sl <= 5.0 and sw <= 3.4 and pl <= 1.4 and pw <= 0.2:
+        return "Setosa"
+    elif sl >= 6.0 and pl >= 4.9:
+        return "Virginica"
+    else:
+        return "Versicolor"
 
-print(sorted_array)
+
+good_predictions_2 = 0
+len_2 = test_set.shape[0]   
+for i in range(len):
+    if classify_iris_2(int(test_set[i, 0]), int(test_set[i, 1]), int(test_set[i, 2]), int(test_set[i, 3])) == test_set[i, 4]:
+        good_predictions_2 = good_predictions_2 + 1
+print(good_predictions_2)
+print(good_predictions_2/len_2*100, "%")
