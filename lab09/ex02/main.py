1d0496fee3cdbfbc0a505c48c90a6a4b2b2e9e9b
mariakoren <maria.korenn@gmail.com>
2024-05-25 11:32:57 +0200

    ex02

--- lab09/ex02/main.py
+++ lab09/ex02/main.py
@@ -6,7 +6,6 @@ from aco import AntColony
 
 plt.style.use("dark_background")
 
-
 COORDS = (
     (20, 52),
     (43, 50),
@@ -15,7 +14,42 @@ COORDS = (
     (29, 90),
     (87, 83),
     (73, 23),
+    (45, 76),
+    (35, 40),
+    (60, 30),
+    (10, 20),
+    (55, 65),
+    (80, 45),
+    (15, 70),
+    (50, 50),
+    (25, 85),
+    (68, 72),
+    (33, 55),
+    (90, 80),
+    (70, 30),
+    (40, 60),
+    (22, 43),
+    (63, 75),
+    (55, 25),
+    (17, 62),
+    (38, 48),
+    (83, 60),
+    (27, 75),
+    (75, 45),
+    (49, 30),
+    (20, 10),
+    (60, 80),
+    (35, 50),
+    (80, 20),
+    (28, 65),
+    (72, 55),
+    (45, 35),
+    (10, 50),
+    (50, 90),
+    (30, 25),
+    (65, 40),
 )
+
 
 
 def random_coord():
@@ -40,9 +74,9 @@ def plot_all_edges():
 
 plot_nodes()
 
-colony = AntColony(COORDS, ant_count=300, alpha=0.5, beta=1.2, 
-                    pheromone_evaporation_rate=0.40, pheromone_constant=1000.0,
-                    iterations=300)
+colony = AntColony(COORDS, ant_count=3000, alpha=0.5, 
+                   beta=1.2, pheromone_evaporation_rate=0.40, 
+                   pheromone_constant=10.0,iterations=30)
 
 optimal_nodes = colony.get_path()
 
@@ -53,4 +87,4 @@ for i in range(len(optimal_nodes) - 1):
     )
 
 
-plt.savefig("plot-aco.png")
+plt.savefig("plot4.png")
---
+++ lab09/ex02/res.md
@@ -0,0 +1,36 @@
+Wierszchołki jakie były podane
+
+plot0
+colony = AntColony(COORDS, ant_count=300, alpha=0.5, 
+                   beta=1.2, pheromone_evaporation_rate=0.40, 
+                   pheromone_constant=1000.0,iterations=300)
+
+231.55104875389208
+
+plot1
+colony = AntColony(COORDS, ant_count=300, alpha=0.5, 
+                   beta=1.2, pheromone_evaporation_rate=0.40, 
+                   pheromone_constant=10.0,iterations=300)
+240.65567155229272
+
+Zmiana wierszchołków
+
+plot2
+
+colony = AntColony(COORDS, ant_count=300, alpha=0.5, 
+                   beta=1.2, pheromone_evaporation_rate=0.40, 
+                   pheromone_constant=10.0,iterations=300)
+
+525.029054052552
+
+plot3
+colony = AntColony(COORDS, ant_count=300, alpha=0.5, 
+                   beta=1.2, pheromone_evaporation_rate=0.40, 
+                   pheromone_constant=100.0,iterations=300)
+555.233670369962
+
+plot4
+colony = AntColony(COORDS, ant_count=3000, alpha=0.5, 
+                   beta=1.2, pheromone_evaporation_rate=0.40, 
+                   pheromone_constant=10.0,iterations=30)
+525.8187579541666
